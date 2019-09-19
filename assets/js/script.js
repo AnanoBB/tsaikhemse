4bfb1037a2c7b89e36b3c67147fd8891e9d29939
AnanoBB <ana.berishvili.1@btu.edu.ge>
2019-09-19 17:18:20 +0400

    updates for burger menu

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let add_button = document.getElementsByClassName("menu_buttons");
+console.log(add_button);
+
+let scr = document.getElementById("more");
+
+scr.addEventListener('click',function(){alert()});
+
+// add_button.addEventListener("click",function(){alert()});
+
+for (let i = 0; i < add_button.length;i++){
+    add_button[i].addEventListener('click',function(){
+        this.classList.toggle('added');
+        
+        if(this.classList.contains('added')){
+            this.textContent = 'დამატებულია';
+        } else{
+            this.textContent = "დამატება";
+        }
+        
+    })
+}
+
